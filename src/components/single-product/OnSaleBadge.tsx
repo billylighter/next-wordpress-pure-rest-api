dba9a23848b618540c9f395d8926eb5160106412
billylighter <vladimir.selevertov@gmail.com>
2025-12-25 22:41:51 +0200

    added icon into OnSaleBadge.tsx

--- src/components/single-product/OnSaleBadge.tsx
+++ src/components/single-product/OnSaleBadge.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import {FaFire} from "react-icons/fa6";
+import {FaFireAlt} from "react-icons/fa";
 
 interface OnSaleBadgeProps {
     text?: string;
@@ -8,9 +10,9 @@ export default function OnSaleBadge({ text }: OnSaleBadgeProps) {
     const textBadge = (text) ? text : 'On sale';
     return (
         <div className="absolute top-3 left-3 z-1">
-            <span className="inline-flex items-center gap-1 rounded-full bg-red-600 px-3 py-1 text-xs font-semibold uppercase tracking-wide text-white shadow">
-                {textBadge}
-            </span>
+            <div className="inline-flex items-center leading-normal gap-1 rounded-full bg-red-600 px-3 py-1 text-xs font-semibold uppercase tracking-wide text-white shadow">
+                <FaFireAlt /><span className={"mt-0.5"}>{textBadge}</span>
+            </div>
         </div>
     );
 }
